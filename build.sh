e424e18fc3305af93ede81c877270db57f3afebc
agnieszka <agnieszka@trdlnk.com>
2023-08-29 10:04:02 +0200

    buisd.sh and .gitignore eddited.

--- build.sh
+++ build.sh
@@ -1,21 +1,41 @@
 #! /bin/sh
 
+# Get the system name using uname
+system_name=$(uname -s)
 
-dir="build"
+# Check if the system is Linux
+# shellcheck disable=SC2039
+if [ "$system_name" = "Linux" ]; then
 
-if [ ! -d $dir ]; then
-    mkdir $dir
-fi
-
-cd build || exit
-
-if [ "$1" = "clean" ]; then
+    cd algorithms || exit
     make clean
+    make
+    cd ../src || exit
+    make clean
+    make
+
+elif [ "$system_name" = "Darwin" ]; then
+
+    echo "This is macOS."
+    dir="build"
+
+    if [ ! -d $dir ]; then
+        mkdir $dir
+    fi
+
+    cd build || exit
+
+    if [ "$1" = "clean" ]; then
+        make clean
+    fi
+
+    cmake ../CMakeLists.txt
+    if ! make; then
+      exit 1
+    fi
+
+    cp ./karst ../tests
+else
+    echo "Unsupported system: $system_name"
 fi
 
-cmake ../CMakeLists.txt
-if ! make; then
-  exit 1
-fi
-
-cp ./karst ../tests
